92ae706ad04d4710f2bdd854ff6ea81deb57c0f8
David Padilla <davidpaag2002@gmail.com>
2022-04-01 22:30:18 +0200

    Trabajando en el routing

---
+++ Cliente (Angular)/ElBancoDeLasPalabras/src/app/pages/auth-module/auth-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  /* {
+    path: 'changePassword',
+    loadChildren: () => import('./pages/change-password/change-password.module').then(m => m.ChangePasswordModule)
+  },
+  {
+    path: 'forgottenPassword',
+    loadChildren: () => import('./pages/forgotten-password/forgotten-password.module').then(m => m.ForgottenPasswordModule)
+  },
+  {
+    path: 'newPassword',
+    loadChildren: () => import('./pages/passactivate/passactivate.module').then(m => m.PassActivatePageModule)
+  }, */
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthRoutingModule { }

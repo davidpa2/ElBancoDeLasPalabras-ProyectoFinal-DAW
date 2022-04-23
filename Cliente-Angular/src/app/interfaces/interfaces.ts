d1e6db2bd17ffc2a1a4d76c9c7b81818bd2e42fa
David Padilla <davidpaag2002@gmail.com>
2022-04-23 21:14:37 +0200

    (Client) Creación de las interfaces BBDD

---
+++ Cliente-Angular/src/app/interfaces/interfaces.ts
@@ -0,0 +1,37 @@
+export interface User {
+    id: number,
+    email: string,
+    password: string
+    name: string,
+    surnames: string,
+    description: string,
+    birthday: string,
+    tlf: string,
+    telegram: string,
+    img: string
+}
+
+export interface Book {
+    id: number,
+    title: string,
+    author: string,
+    description: string,
+    state: string,
+    price: string,
+    img: string,
+    idBuyer: User,
+    idUser: User
+}
+
+export interface Exchange {
+    id: number,
+    idPetitioner: User,
+    idBookP: Book,
+    idTitular: User,
+    idBookT: Book,
+    date: string
+}
+
+export interface TokenJWT {
+    jwt: any
+}

18d7d6d13d42696dfa4d793fe0bb39022b432cd7
David Padilla <davidpaag2002@gmail.com>
2022-05-03 22:20:05 +0200

    (Client) Book service

---
+++ Cliente-Angular/src/app/services/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Book, User } from '../interfaces/interfaces';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  private url: string = "http://localhost:8080";
+
+  constructor(private http: HttpClient) { }
+
+  uploadBook(title: string, author: string,  description: string, state: number, price: string, img: string, user: User): Observable<Book> {
+    var jsonObject = {
+      title: title,
+      author: author,
+      description: description,
+      state: state,
+      price: price,
+      img: img,
+      user: user
+    };
+    // Envío la petición http y devuelvo el Observable, para que cualquiera pueda subscribirse.
+    return this.http.post<Book>(this.url + '/uploadBook', jsonObject);
+  }
+}

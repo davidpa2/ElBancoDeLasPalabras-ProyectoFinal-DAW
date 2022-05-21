f6ddf5bd775d3d0a4c1f8975baaf713a08d2e625
David Padilla <davidpaag2002@gmail.com>
2022-05-21 18:06:14 +0200

    (Client) Validación formulario recuperar contraseña

--- Cliente-Angular/src/app/pages/auth-module/pages/forgotten-password/forgotten-password.component.ts
+++ Cliente-Angular/src/app/pages/auth-module/pages/forgotten-password/forgotten-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotten-password',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgottenPasswordComponent implements OnInit {
 
+  recoveryForm!: FormGroup;
+  submitted = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.recoveryForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$')])
+    })
+  }
+
+  recovery() {
+    this.submitted = true;
+
+    if (this.recoveryForm.valid) {
+      console.log('SA MANDAO');
+      
+    }
+  }
+
+  get myForm() {
+    return this.recoveryForm.controls;
   }
 
 }

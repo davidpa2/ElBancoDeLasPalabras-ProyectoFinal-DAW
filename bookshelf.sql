16c6a0c27f0161a59ca19863770e6d9f785318ea
David Padilla <davidpaag2002@gmail.com>
2022-06-03 19:40:31 +0200

    Base de datos definitiva

--- bookshelf.sql
+++ bookshelf.sql
@@ -1,122 +1,104 @@
--- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: bookshelf
--- ------------------------------------------------------
--- Server version	8.0.23
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `book`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema bookshelf
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `book`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `book` (
-  `id` int NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `state` varchar(45) DEFAULT NULL,
-  `price` varchar(45) DEFAULT NULL,
-  `img` longblob,
-  `idBuyer` int DEFAULT NULL,
-  `idUser` int NOT NULL,
+-- -----------------------------------------------------
+-- Schema bookshelf
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `bookshelf` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `bookshelf` ;
+
+-- -----------------------------------------------------
+-- Table `bookshelf`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bookshelf`.`user` (
+  `id` INT NOT NULL,
+  `email` VARCHAR(150) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(150) NULL DEFAULT NULL,
+  `surnames` VARCHAR(200) NULL DEFAULT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `birthday` VARCHAR(45) NULL DEFAULT NULL,
+  `tlf` VARCHAR(12) NULL DEFAULT NULL,
+  `telegram` VARCHAR(150) NULL DEFAULT NULL,
+  `img` LONGBLOB NULL DEFAULT NULL,
+  `rating` FLOAT NULL DEFAULT NULL,
+  `location` VARCHAR(200) NULL DEFAULT NULL,
+  `recoveryKey` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `idUser_idx` (`idUser`),
-  CONSTRAINT `idUser` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `book`
---
 
-LOCK TABLES `book` WRITE;
-/*!40000 ALTER TABLE `book` DISABLE KEYS */;
-/*!40000 ALTER TABLE `book` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `exchange`
---
-
-DROP TABLE IF EXISTS `exchange`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `exchange` (
-  `id` int NOT NULL,
-  `idPetitioner` int DEFAULT NULL,
-  `idBookP` int DEFAULT NULL,
-  `idTitular` int DEFAULT NULL,
-  `idBookT` int DEFAULT NULL,
-  `date` varchar(45) DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `bookshelf`.`book`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bookshelf`.`book` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `author` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(500) NULL DEFAULT NULL,
+  `state` INT NULL DEFAULT NULL,
+  `price` VARCHAR(45) NULL DEFAULT NULL,
+  `img` LONGBLOB NULL DEFAULT NULL,
+  `reserved` INT NULL DEFAULT NULL,
+  `buyer_id` INT NULL DEFAULT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `idBookP` FOREIGN KEY (`id`) REFERENCES `book` (`id`),
-  CONSTRAINT `idBookT` FOREIGN KEY (`id`) REFERENCES `book` (`id`),
-  CONSTRAINT `idPetitioner` FOREIGN KEY (`id`) REFERENCES `user` (`id`),
-  CONSTRAINT `idTitular` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  INDEX `user_id` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `bookshelf`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `exchange`
---
 
-LOCK TABLES `exchange` WRITE;
-/*!40000 ALTER TABLE `exchange` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exchange` ENABLE KEYS */;
-UNLOCK TABLES;
+-- -----------------------------------------------------
+-- Table `bookshelf`.`exchange`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bookshelf`.`exchange` (
+  `id` INT NOT NULL,
+  `userP_id` INT NULL DEFAULT NULL,
+  `bookP_id` INT NULL DEFAULT NULL,
+  `idUserO` INT NOT NULL,
+  `idBookO` INT NOT NULL,
+  `date` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `bookp_id` (`bookP_id` ASC) VISIBLE,
+  INDEX `userP_id` (`userP_id` ASC) VISIBLE,
+  CONSTRAINT `bookp_id`
+    FOREIGN KEY (`bookP_id`)
+    REFERENCES `bookshelf`.`book` (`id`),
+  CONSTRAINT `userP_id`
+    FOREIGN KEY (`userP_id`)
+    REFERENCES `bookshelf`.`user` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Table structure for table `user`
---
 
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` int NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `name` varchar(150) DEFAULT NULL,
-  `surnames` varchar(200) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `birthday` varchar(45) DEFAULT NULL,
-  `tlf` varchar(12) DEFAULT NULL,
-  `telegram` varchar(150) DEFAULT NULL,
-  `img` longblob,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Table `bookshelf`.`hibernate_sequence`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bookshelf`.`hibernate_sequence` (
+  `next_val` BIGINT NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `user`
---
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'david@gmail.com','1234','David','Padilla Agulera','Hola, soy yo','04/03/02','666','david',NULL);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-04-18 13:01:11
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
